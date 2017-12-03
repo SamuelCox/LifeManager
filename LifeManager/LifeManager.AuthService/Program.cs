b35da44c418da547a6b13d17733dc28151504f07
samuelcox <sc630@kent.ac.uk>
2017-12-03 21:35:41 +0000

    Added additional needed projects so I can start to work on auth, will probably get rid of auth service though.

---
+++ LifeManager/LifeManager.AuthService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LifeManager.AuthService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LifeManager/LifeManager.Messages/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LifeManager.Messages
+{
+    public class Class1
+    {
+    }
+}
